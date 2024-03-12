5916e20c8582fdd385a6ad1069cd1957c7451adf
tahfeezmizan <mizanahmed5316@gmial.com>
2024-03-12 23:50:59 +0600

    add country flag

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,10 +2,11 @@ import './Country.css'
 
 const Country = ({country}) => {
     console.log(country)
-    const {name} = country;
+    const {name, flags} = country;
     return (
         <div className="country">
             <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
         </div>
     );
 };
