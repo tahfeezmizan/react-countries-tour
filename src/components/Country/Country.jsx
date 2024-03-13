3caf404115b423e7555f519627b104f677b8da8d
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-03-12 23:26:51 -0700

    flag add dynamiclly  Country.jsx

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({country, handleVisitedCountry}) => {
-    const {name, flags, population, area, cca3} = country;
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+    const { name, flags, population, area, cca3 } = country;
 
     const [visited, setVisited] = useState(false);
 
@@ -18,7 +18,8 @@ const Country = ({country, handleVisitedCountry}) => {
             <p>Area: {area}</p>
             <p>Code: {cca3}</p>
             <button onClick={() => handleVisitedCountry(country)}>Mark Visted</button> <br />
-            <button onClick={handleVisited}>{visited ?  'Visited' : 'Going'}</button>
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button> <br />
+            <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
             {visited ? 'I have visited this country' : 'I want to Visite'}
         </div>
     );
