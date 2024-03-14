8d6e78979ec1085cec5c0f57237f2f68b8f1f6e0
Tahfeez Mizan <mizanahmed5316@gmail.com>
2024-03-14 06:53:46 -0700

    create json file on local stroge and fetch data App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import './App.css'
-import Countries from './components/Countries/Countries'
+import Countries from './components/Countries/Countries' 
+import { useEffect, useState } from 'react'
+import Watch from './components/Watch/Watch'
+
 
 function App() {
+  const [watches, setWatches] = useState([]);
+
+  useEffect(() => {
+    fetch('watches.json')
+    .then(res => res.json())
+    .then(data => setWatches(data))
+  }, 
+  []);
+
+  // const watches = [
+  //   {id: 1, name: 'Apple Watch', price: '1500'},
+  //   {id: 2, name: 'Samsung Watch', price: '1200'},
+  //   {id: 3, name: 'Nokia Smart Watch', price: '1000'}
+  // ]
 
   return (
     <>
+      <h1>Vite + React</h1>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
       <h1>React Wold Tour</h1>
       <Countries></Countries>
-
     </>
   )
 }
